6d2d9afdb775035ba4b4b136fbfadfe7bb91eba8
Roman Makeev <makeevrserg@gmail.com>
2023-02-12 20:40:04 +0300

    fixed jvm desktop dispatchers

--- ktx-core/src/jvmMain/kotlin/com/makeevrserg/mobilex/ktx_core/platform/KDispatchers.kt
+++ ktx-core/src/jvmMain/kotlin/com/makeevrserg/mobilex/ktx_core/platform/KDispatchers.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 actual object KDispatchers: KotlinDispatchers {
-    actual override val Main: CoroutineDispatcher = Dispatchers.Main
+    actual override val Main: CoroutineDispatcher = Dispatchers.Default
     actual override val IO: CoroutineDispatcher = Dispatchers.IO
     actual override val Default: CoroutineDispatcher = Dispatchers.Default
     actual override val Unconfined: CoroutineDispatcher = Dispatchers.Unconfined
