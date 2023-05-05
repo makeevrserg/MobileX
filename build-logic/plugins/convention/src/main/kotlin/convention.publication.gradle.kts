652263a5cd18156b20287ab4175f57e758d7ba32
Roman Makeev <makeevrserg@gmail.com>
2023-05-05 21:59:39 +0300

    - Attempt to fix autopublish

--- build-logic/plugins/convention/src/main/kotlin/convention.publication.gradle.kts
+++ build-logic/plugins/convention/src/main/kotlin/convention.publication.gradle.kts
@@ -1,41 +1,54 @@
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.registering
 import org.gradle.kotlin.dsl.signing
 import org.gradle.kotlin.dsl.withType
 import java.util.*
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform")
-    id("org.gradle.maven-publish")
-    id("signing")
+    `maven-publish`
+    signing
 }
-kotlin {
-    android {
-        publishAllLibraryVariants()
-        publishLibraryVariantsGroupedByFlavor = true
-    }
-}
-publishing {
-    repositories.maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
-        name = "OSSRH"
 
-        credentials {
-            username = System.getenv("OSSRH_USER")
-            password = System.getenv("OSSRH_KEY")
+// Grabbing secrets from local.properties file or from environment variables, which could be used on CI
+val secretPropsFile = project.rootProject.file("local.properties")
+val properties = Properties().apply { load(secretPropsFile.reader()) }
+
+val SIGNING_KEY_ID = System.getenv("SIGNING_KEY_ID") ?: properties.getProperty("signing.keyId")
+val SIGNING_PASSWORD = System.getenv("SIGNING_PASSWORD") ?: properties.getProperty("signing.password")
+val OSSRH_USERNAME = System.getenv("OSSRH_USERNAME") ?: properties.getProperty("ossrhUsername")
+val OSSRH_PASSWORD = System.getenv("OSSRH_PASSWORD") ?: properties.getProperty("ossrhPassword")
+val SIGNING_KEY = System.getenv("SIGNING_KEY") ?: properties.getProperty("signing.base64")
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = OSSRH_USERNAME
+                password = OSSRH_PASSWORD
+            }
         }
     }
     publications.withType<MavenPublication> {
-        pom {
-            name.set(libs.versions.library.name)
-            description.set(libs.versions.library.description)
-            url.set("https://github.com/makeevrserg/MobileX")
 
+        artifact(javadocJar.get())
+        pom {
+            name.set(libs.versions.library.name.get())
             group = libs.versions.library.group.get()
             version = libs.versions.library.version.string.get()
+            description.set(libs.versions.library.description.get())
+            url.set("https://github.com/makeevrserg/MobileX")
 
             licenses {
                 license {
                     name.set("MIT")
-                    distribution.set("repo")
                     url.set("https://github.com/makeevrserg/MobileX/blob/main/LICENSE.md")
                 }
             }
@@ -55,14 +68,18 @@ publishing {
     }
 }
 
+tasks.create("Base64FromGPG") {
+    val SIGNING_SECRET_KEY_RING_FILE = properties.getProperty("signing.secretKeyRingFile")
+    val SIGNING_KEY = Base64.getEncoder().encodeToString(File(SIGNING_SECRET_KEY_RING_FILE).readBytes())
+    File("./key.txt").apply {
+        if (!exists()) {
+            createNewFile()
+            writeText(SIGNING_KEY)
+        }
+    }
+}
+
 signing {
-    val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
-    val signingPassword: String? = System.getenv("SIGNING_PASSWORD")
-    val signingKey: String? = System.getenv("SIGNING_KEY")?.let { base64Key ->
-        String(Base64.getDecoder().decode(base64Key))
-    }
-    if (signingKeyId != null) {
-        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-        sign(publishing.publications)
-    }
+    useInMemoryPgpKeys(SIGNING_KEY_ID, SIGNING_KEY, SIGNING_PASSWORD)
+    sign(publishing.publications)
 }
