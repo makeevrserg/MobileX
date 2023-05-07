377dd6680fc5b402bf802a000ef035132260e058
Roman Makeev <makeevrserg@gmail.com>
2023-05-07 16:21:46 +0300

    - Removed KDispatchers from ios/jvm/android, it's now in common - Split UseCases - Updated version

--- ktx-core/src/commonMain/kotlin/com/makeevrserg/mobilex/core/platform/KDispatchers.kt
+++ ktx-core/src/commonMain/kotlin/com/makeevrserg/mobilex/core/platform/KDispatchers.kt
@@ -1,12 +1,15 @@
 package com.makeevrserg.mobilex.core.platform
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+
 /**
  * Workaround for using IO dispatcher with KMM ios sourceSet
  */
-expect object KDispatchers : KotlinDispatchers {
-    override val Main: CoroutineDispatcher
-    override val IO: CoroutineDispatcher
-    override val Default: CoroutineDispatcher
-    override val Unconfined: CoroutineDispatcher
+object KDispatchers : KotlinDispatchers {
+    override val Main: CoroutineDispatcher = Dispatchers.Main
+    override val IO: CoroutineDispatcher = Dispatchers.IO
+    override val Default: CoroutineDispatcher = Dispatchers.Default
+    override val Unconfined: CoroutineDispatcher = Dispatchers.Unconfined
 }
