3460f45a378da9af2b500c9a97ee67ffcc7a1d04
Roman Makeev <makeevrserg@gmail.com>
2023-07-11 18:52:41 +0300

    fix: gradle plugin order

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
 subprojects.forEach {
     it.apply(plugin = "ru.astrainteractive.gradleplugin.dokka.module")
     it.apply(plugin = "ru.astrainteractive.gradleplugin.publication")
+    it.apply(plugin = "ru.astrainteractive.gradleplugin.stub.javadoc")
     it.plugins.withId("org.jetbrains.kotlin.jvm") {
         it.apply(plugin = "ru.astrainteractive.gradleplugin.java.core")
     }
-    it.apply(plugin = "ru.astrainteractive.gradleplugin.stub.javadoc")
     it.plugins.withId("com.android.library") {
         it.apply(plugin = "ru.astrainteractive.gradleplugin.android.core")
     }
